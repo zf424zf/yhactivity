6ea77fe1cfa60bed452c4431a0308c88865f7cae
fan zhang <473994529@qq.com>
2017-09-18 09:08:44 +0800

    a

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -80,7 +80,7 @@ class ImageController extends Controller
 
     public function indexView()
     {
-         return $data = (new FileList())->videoList(1, '');
+         $data = (new FileList())->videoList(1, '');
         $newData = [3=>$data[3],2=>$data[2],4=>$data[4],1=>$data[1]];
         return view('photo.index', ['data' => $newData]);
     }
