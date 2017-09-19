0699e33e64d92f350cb74ccab882b7920767a79b
fan zhang <473994529@qq.com>
2017-09-19 15:05:03 +0800

    a

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -127,6 +127,7 @@
     </div>
     <script type="text/javascript">
         $(function(){
+            $('.video').play();
 //            document.addEventListener("WeixinJSBridgeReady", function (){
 //                video.play();
 //                video.pause();
