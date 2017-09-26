f4ade5f7391c0423f2212431c982857365e20de1
fan zhang <473994529@qq.com>
2017-09-26 20:26:40 +0800

    a

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -163,5 +163,17 @@
     }
 </script>
 @yield('content')
+<script>
+    var _mtac = {};
+    (function() {
+        var mta = document.createElement("script");
+        mta.src = "http://pingjs.qq.com/h5/stats.js?v2.0.2";
+        mta.setAttribute("name", "MTAH5");
+        mta.setAttribute("sid", "500528860");
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(mta, s);
+    })();
+</script>
+
 </body>
 </html>
