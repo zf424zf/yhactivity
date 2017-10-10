8d0ec0e95b630f36d1fed59b0b930d99b7d139e6
fan zhang <473994529@qq.com>
2017-10-10 08:43:35 +0800

    a

---
+++ app/Admin/Controllers/WinController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 70427
+ * Date: 2017/10/3
+ * Time: 16:51
+ */
+
+namespace App\Admin\Controllers;
+
+
+use App\Http\Controllers\Controller;
+
+class WinController extends Controller
+{
+
+}
